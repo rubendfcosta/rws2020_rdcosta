e66ae77acc74b4b96b88fa8c49324be23b222e87
rubendfcosta <rubencsta@gmail.com>
2020-02-21 10:33:03 +0000

    Update player rdcosta hunting mode activated

--- player_rdcosta/src/player_node.py
+++ player_rdcosta/src/player_node.py
@@ -149,7 +149,7 @@ class Player:
         max_vel, max_angle = msg.turtle, math.pi / 30
 
         if msg.blue_alive:  # PURSUIT MODE: Follow any blue player (only if there is at least one blue alive)
-            target = msg.blue_alive[0]  # select the first alive blue player (I am hunting blue)
+            target = msg.blue_alive[0]  #  select the first alive blue player (I am hunting blue)
             distance, angle = getDistanceAndAngleToTarget(self.listener,
                                                           self.player_name, target)
 
